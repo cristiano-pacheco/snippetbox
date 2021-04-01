005b568df4fd71e1ffc1d5d060417960e700c260
Cristiano Pacheco <chris.spb25@gmail.com>
2021-04-01 12:48:15 -0300

    chapter 8

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,15 +5,13 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/cristiano-pacheco/snippetbox/pkg/forms"
 	"github.com/cristiano-pacheco/snippetbox/pkg/models"
 )
 
-// Define a templateData type to act as the holding structure for
-// any dynamic data that we want to pass to our HTML templates.
-// At the moment it only contains one field, but we'll add more
-// to it as the build progresses.
 type templateData struct {
 	CurrentYear int
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
