0046da7c08f1a2e5b1684564e6eed938b33e2c2b
Cristiano Pacheco <chris.spb25@gmail.com>
2021-04-01 13:50:58 -0300

    chapter 9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,16 +7,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/cristiano-pacheco/snippetbox/pkg/models/mysql" // New import
+	"github.com/cristiano-pacheco/snippetbox/pkg/models/mysql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
-// Add a templateCache field to the application struct.
+// Add a new session field to the application struct.
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
+	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
@@ -24,6 +27,10 @@ type application struct {
 func main() {
 	dsn := flag.String("dsn", "root:root@/snippetbox?parseTime=true", "MySQL data source name")
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command-line flag for the session secret (a random key which
+	// will be used to encrypt and authenticate session cookies). It should be 32
+	// bytes long.
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -41,10 +48,16 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
-	// And add it to the application dependencies.
+	// Use the sessions.New() function to initialize a new session manager,
+	// passing in the secret key as the parameter. Then we configure it so
+	// sessions always expires after 12 hours.
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
+		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
